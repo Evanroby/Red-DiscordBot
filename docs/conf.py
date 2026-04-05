0d655aa34a7f33c4ae769fe8c0a2bf6af63bb8b0
Evanroby <107794516+Evanroby@users.noreply.github.com>
2026-04-05 13:01:49 +0200

    fix warnings

--- docs/conf.py
+++ docs/conf.py
@@ -158,15 +158,6 @@ html_context = {
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    "**": [
-        "about.html",
-        "navigation.html",
-        "relations.html",  # needs 'show_related': True theme option to display
-        "searchbox.html",
-        "donate.html",
-    ]
-}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
